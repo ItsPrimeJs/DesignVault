cd79445e680e07ff56031feb9717eefaca0c4f87
ItsPrimeJs <sujalshelar918@gmail.com>
2024-08-25 14:37:59 +0530

    User and post schema models added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const userModel = require("./users");
+const postModel = require("./post");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
+/* Create a new user */
+router.get("/createuser", async function (req, res, next) {
+  let createdUser = await userModel.create({
+    username: "Sujal",
+    password: "Sujal123",
+    posts: [],
+    email: "Sujal@gmail.com", // Corrected email format
+    fullname: "Sujal Shailesh Shelar",
+  });
+
+  res.send(createdUser);
+});
+
+router.get("/createpost", async function (req, res, next) {
+  let createdPost =  await  postModel.create({
+    postText: "HEllo everyone"
+  });
+  res.send(createdPost);
+});
 module.exports = router;
